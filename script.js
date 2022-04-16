dbb041f2be26b5d1254339b13191975471e5ed65
NogitBR <nogitoficial@gmail.com>
2022-04-16 16:33:01 -0300

    Almost! 50%

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-console.log("hello world!")
+console.log("hello!");
+
+$(function () {
+  $(".carousel").carousel({ interval: 100 });
+});
+
+const previ = () => {
+  console.log("prev!");
+  $(".carousel").carousel("prev");
+};
+
+const next = () => {
+  console.log("prev!");
+  $(".carousel").carousel(3);
+};
