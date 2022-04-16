05d2bfdc1302ff45ddbdccd075c68dead864975e
NogitBR <nogitoficial@gmail.com>
2022-04-16 16:40:08 -0300

    final

--- script.js
+++ script.js
@@ -1,15 +1,1 @@
 console.log("hello!");
-
-$(function () {
-  $(".carousel").carousel({ interval: 100 });
-});
-
-const previ = () => {
-  console.log("prev!");
-  $(".carousel").carousel("prev");
-};
-
-const next = () => {
-  console.log("prev!");
-  $(".carousel").carousel(3);
-};
