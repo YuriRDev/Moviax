93c7c6fb9c36afcf56a7fb7962c647bfc156fc15
NogitBR <nogitoficial@gmail.com>
2022-04-18 13:12:54 -0300

    Removed images placeholder!

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
-console.log("hello!");
+const scrollDestaque = () => {
+  document
+    .getElementById("destaque")
+    .scrollIntoView({ behavior: "smooth", block: "end" });
+};
+
+const scrollAvaliacoes = () => {
+  document
+    .getElementById("avaliacoes")
+    .scrollIntoView({ behavior: "smooth", block: "end" });
+};
+
+const scrollEntrevistas = () => {
+  document
+    .getElementById("entrevistas")
+    .scrollIntoView({ behavior: "smooth", block: "end" });
+};
+
+const scrollNovidades = () => {
+  document
+    .getElementById("novidades")
+    .scrollIntoView({ behavior: "smooth", block: "end" });
+};
