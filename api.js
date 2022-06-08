c170ecda50c861bbc636180db1763263293b206a
YuriRDev <nogitoficial@gmail.com>
2022-06-08 19:50:19 -0300

    API update

--- api.js
+++ api.js
@@ -69,6 +69,8 @@ const loadMoreMovies = () => {
   <img src="https://image.tmdb.org/t/p/w300_and_h450_bestv2/uEPJQY1kEEz9XoZZ8rP6p9JUrmv.jpg" class="movie-display-image">
 </div>`;
 
+  const queryChildren = ``
+
   document
     .querySelector("#destaque > div > div > div.btn-sec-container")
     .remove();
