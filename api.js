75a4885591c4a2492a1a3bf9fab9f9d6f1024ad9
NogitBR <nogitoficial@gmail.com>
2022-06-16 12:59:40 -0300

    ♥ Routed!

--- api.js
+++ api.js
@@ -166,7 +166,7 @@ const displaySearchMovies = (info) => {
   document.querySelector("#movies-display");
   info.results.map((item) => {
     const htmlAppend = `
-    <div class="col-3 movie-search">
+    <div class="col-3 movie-search" onclick=location.href='/html/detalhes.html?id=${item.id}' >
     <img
       src="https://image.tmdb.org/t/p/w300_and_h450_bestv2/${item.poster_path}"
     />
